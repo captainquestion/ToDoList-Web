2538065a25939863fc2bb282133e0a3d0536c212
captainquestion <canberk.yilmaz439@gmail.com>
2023-04-12 22:05:55 -0400

    Changes been made

--- app.js
+++ app.js
@@ -165,12 +165,12 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
 
 
-app.listen(port, function () {
-  console.log("Server has started successfully.");
-});
+app.listen(process.env.PORT || 3000, function () { 
+  console.log("Server started.");
+   }); 
